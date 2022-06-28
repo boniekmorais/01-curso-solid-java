56339384ea13420e3a6f0c248b89071164025b38
Boniek Morais <boniek.morais@gmail.com>
2022-06-27 21:02:26 -0300

    Commit inicial do projeto

---
+++ src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
@@ -0,0 +1,25 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeriodicidadeEntreReajustes implements ValidacaoReajuste {
+
+    @Override
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes não permitido!");
+        }
+    }
+
+}
